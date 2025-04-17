9f614c91723222c965628e9af1c97c5333b1591b
tmura-lu <luccagbat@gmail.com>
2025-04-17 17:05:00 -0300

    Adicionadas classes A e B com métodos MA1, MA2, MB1, MB2

---
+++ src/classe_a.py
@@ -0,0 +1,23 @@
+class A:
+    def __init__(self):
+        self.A1 = 0
+        self.A2 = 0.0
+
+    def get_A1(self):
+        return self.A1
+
+    def set_A1(self, value):
+        self.A1 = value
+
+    def get_A2(self):
+        return self.A2
+
+    def set_A2(self, value):
+        self.A2 = value
+
+    def MA1(self):
+        print("MA1")
+
+    def MA2(self):
+        print("MA2")
+
---
+++ src/classe_b.py
@@ -0,0 +1,23 @@
+class B:
+    def __init__(self):
+        self.B1 = 0
+        self.B2 = 0.0
+
+    def get_B1(self):
+        return self.B1
+
+    def set_B1(self, value):
+        self.B1 = value
+
+    def get_B2(self):
+        return self.B2
+
+    def set_B2(self, value):
+        self.B2 = value
+
+    def MB1(self):
+        print("MB1")
+
+    def MB2(self):
+        print("MB2")
+
