bbdfb61ef5159874f289a80470c05a5dc8dfd0e5
tmura-lu <luccagbat@gmail.com>
2025-04-17 17:25:53 -0300

    Criação da Classe C

--- src/classe_a.py
+++ src/classe_a.py
@@ -20,4 +20,7 @@ class A:
 
     def MA2(self):
         print("MA2")
+    
+    def MA3(self):
+        print("Alteração a classe A partir do clone")
 
---
+++ src/classe_c.py
@@ -0,0 +1,23 @@
+class C:
+    def __init__(self):
+        self.C1 = ""
+        self.C2 = 0
+
+    def get_C1(self):
+        return self.C1
+
+    def set_C1(self, value):
+        self.C1 = value
+
+    def get_C2(self):
+        return self.C2
+
+    def set_C2(self, value):
+        self.C2 = value
+
+    def MC1(self):
+        print("MC1")
+
+    def MC2(self):
+        print("MC2")
+
